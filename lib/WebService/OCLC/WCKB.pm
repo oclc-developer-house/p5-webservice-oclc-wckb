77090363f01c05ceb01f337e4f70ff73631c5cf6
Brian Cassidy <bricas@cpan.org>
2014-02-06 18:15:38 -0400

    stub docs and tests to match

--- lib/WebService/OCLC/WCKB.pm
+++ lib/WebService/OCLC/WCKB.pm
@@ -93,3 +93,36 @@ sub get_entries_by_collection {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+WebService::OCLC::WCKB - Library to access the OCLC WorldCat Knowledge Base API
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<http://www.oclc.org/developer/services/worldcat-knowledge-base-api>
+
+=back
+
+=head1 AUTHOR
+
+Brian Cassidy E<lt>bricas@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2014 by Brian Cassidy
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. 
+
+=cut
